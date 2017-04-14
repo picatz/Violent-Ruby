613be70d5859dcac02e7b1e8430d6833ed5c8dd5
Kent Gruber <kgruber1@emich.edu>
2017-04-14 19:53:57 -0400

    Require banner grabber to actually use it.

--- lib/violent_ruby.rb
+++ lib/violent_ruby.rb
@@ -1,10 +1,9 @@
+require "violent_ruby/banner_grabber/banner_grabber"
 require "violent_ruby/ftp_brute_forcer/ftp_brute_forcer"
 require "violent_ruby/ssh_brute_forcer/ssh_brute_forcer"
 require "violent_ruby/vulnerability_scanner/vulnerability_scanner"
 require "violent_ruby/unix_password_cracker/unix_password_cracker"
 require "violent_ruby/version"
-
-
 
 module ViolentRuby
   # Your code goes here...
